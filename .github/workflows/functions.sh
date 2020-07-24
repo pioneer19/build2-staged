d3fe0584b46d373d2e9dce1f7c56bba82c6fd770
Alex Syrnikov <pioneer19@post.cz>
2020-07-25 02:05:57 +0300

    transition to only debian dir without upstream

--- .github/workflows/functions.sh
+++ .github/workflows/functions.sh
@@ -1,68 +1,106 @@
 #!/bin/bash
 
-function import_orig_tar_xz()
+function create_data_json()
 {
-    echo "=== git remote -v"
-    git remote -v
+  VERSION_DATE=$(echo "${FULL_PACKAGE_VERSION}" | sed "s/[^-]*-a.0.\([0-9]*\).*/\1/")
 
-    git checkout upstream/staged
-    git checkout -b ubuntu/focal_new origin/ubuntu/focal
-    git config --global user.email "pioneer19@mailo.com"
-    git config --global user.name  "Alex Syrnikov"
-    gbp import-orig -v --debian-branch=ubuntu/focal_new --upstream-branch=upstream/staged --no-interactive $(ls -1 ../*orig*)
-    echo "=== git push origin upstream/staged"
-    git push origin upstream/staged
+  cat << DATA_JSON > ../data.json
+{
+  "version" : "${VERSION}",
+  "major_minor_version": "${MAJOR_MINOR_VERSION}",
+  "version_date" : "${VERSION_DATE}",
+  "rfc_email_date": "$(date -R)"
+}
+DATA_JSON
+
+  echo "=== ../data.json ==="
+  cat ../data.json
 }
 
-function get_full_package_version_from_orig()
+function create_bash_config()
 {
-    export FULL_PACKAGE_VERSION=$(ls -1 ../*orig* | head -1 | sed 's/\(.*_\)\(.*\)\(\.orig.*\)/\2/');
-    echo FULL_PACKAGE_VERSION = $FULL_PACKAGE_VERSION;
+  cat << DATA_JSON > ../runtime_config.sh
+UPSTREAM_FILE_NAME="$UPSTREAM_FILE_NAME"
+FULL_PACKAGE_VERSION="$FULL_PACKAGE_VERSION"
+VERSION="$VERSION"
+MAJOR_MINOR_VERSION="$MAJOR_MINOR_VERSION"
+DATA_JSON
+
+  echo "=== ../runtime_config.sh ==="
+  cat ../runtime_config.sh
 }
 
-function update_changelog()
+function create_runtime_config()
 {
-    local package_name=$1
+  local USCAN_LOG=$1
 
-    get_full_package_version_from_orig
-    echo "=== env"
-    env
-    cat << CHANGELOG > debian/changelog
-$package_name (${FULL_PACKAGE_VERSION}-20.04ppa1) focal; urgency=low
+  UPSTREAM_FILE_NAME=$(grep "Filename.*for downloaded file" ${USCAN_LOG} | sed "s/.*downloaded file:[ ]*//")
+  echo UPSTREAM_FILE_NAME = $UPSTREAM_FILE_NAME
 
-  * New upstream Release
+  FULL_PACKAGE_VERSION=$(ls -1 ../*orig.tar.xz | head -1 | sed 's/\(.*_\)\(.*\)\(\.orig.*\)/\2/');
+  echo FULL_PACKAGE_VERSION = $FULL_PACKAGE_VERSION;
 
- -- Alex Syrnikov <pioneer19@mailo.com>  $(date -R)
-CHANGELOG
-    echo "=== debian/changelog"
-    cat debian/changelog
+  VERSION=$(echo "${FULL_PACKAGE_VERSION}" | sed "s/\([^-]*\)-.*/\1/")
+  echo VERSION = $VERSION
 
-    git add debian
-    git commit -m "updated changelog"
-    echo "=== git remote -v"
-    git remote -v
-    git push origin ubuntu/focal_new
+  MAJOR_MINOR_VERSION=$(echo "${FULL_PACKAGE_VERSION}" | sed "s/\([^.]*\).\([^.]*\).\([^.]*\).*/\1.\2/")
+  echo MAJOR_MINOR_VERSION = $MAJOR_MINOR_VERSION
+
+  create_data_json
+  create_bash_config
 }
 
-function update_control()
+function render_templates()
 {
-  get_full_package_version_from_orig
-  sed -i "s/([-.=0-9a-f]*20.04ppa[0-9]*)/(=${FULL_PACKAGE_VERSION}-20.04ppa1)/" debian/control
-  echo "=== cat debian/control"
-  cat debian/control
+  . ../runtime_config.sh
+
+  pushd .
+  cd debian/templates
+
+  ls -1 *0.13* | while read f;
+  do
+    new_f=$(echo $f|sed s/0\.13/$MAJOR_MINOR_VERSION/);
+    mv "$f" "$new_f";
+  done
+  echo "=== updated templates ==="
+  ls -la .
+
+  npm install -g ejs
+  echo "=== post ejs install ==="
+  ls -1 *ejs | while read f;
+  do
+    out_file=$(echo "$f"| sed "s/\(.*\).ejs/\1/")
+    npx ejs -f ../../../data.json "$f" -o ../"$out_file"
+    chmod --reference="$f" ../"$out_file"
+  done
+  popd
+}
+
+function update_debian_dir_push_to_focal_new()
+{
+  render_templates
+  rm -rf debian/templates
+
+  echo "=== git status ==="
   git status
 
-  git add debian/control
-  git commit -m "updated debian/control"
-  git push origin ubuntu/focal_new
+  git checkout -b ubuntu/focal_new
+  git config --global user.email "pioneer19@mailo.com"
+  git config --global user.name  "Alex Syrnikov"
+  git add debian
+  git commit -m "updated debian dir from templates"
+  echo "=== git remote -v"
+  git remote -v
+  git push origin +ubuntu/focal_new
 }
 
 function update_symbols()
 {
-    echo "updating symbols"
+    echo "=== updating symbols ==="
     git clean -dxf
     git restore .
-    pkgkde-symbolshelper batchpatch -v 0.13.0 ../build.log
+    . ../runtime_config.sh
+    pkgkde-symbolshelper batchpatch -v ${VERSION} ../build.log
     local files_count=$(git status -s debian/*symbols | wc -l)
     if [[ $files_count -eq 0 ]]
     then
@@ -77,7 +115,7 @@ function update_symbols()
         git add "$f"
         echo "=== updated symbols $f"
     done
-    git commit -m "updated symbols files"
+    git commit -m "updated symbol files"
     git push origin ubuntu/focal_new
 
     echo "git status"
@@ -90,8 +128,7 @@ function install_debs_and_remove_files()
 
     dpkg -i ../*.deb
     rm ../*deb
-    get_full_package_version_from_orig
-    rm ../${package_name}_${FULL_PACKAGE_VERSION}-*
+    rm ../${package_name}_*
     echo "=== ls -la ../"
     ls -la ../
 }
